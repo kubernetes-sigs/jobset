9a2bb05347ee3c4e4cae25a1f24e9b738d08880f
Daniel Vega-Myhre <105610547+danielvegamyhre@users.noreply.github.com>
2023-04-04 15:39:39 -0700

    Add JobSet types (#2)
    * add jobset types
    
    * remove JobSetStatus.Active
    
    * address comments

--- api/v1alpha1/jobset_types.go
+++ api/v1alpha1/jobset_types.go
@@ -14,6 +14,7 @@ limitations under the License.
 package v1alpha1
 
 import (
+	batchv1 "k8s.io/api/batch/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
@@ -22,38 +23,45 @@ import (
 
 // JobSetSpec defines the desired state of JobSet
 type JobSetSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of JobSet. Edit jobset_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Jobs is the group of jobs that will form the set.
+	// +listType=map
+	// +listMapKey=name
+	Jobs []JobTemplate `json:"jobs"`
 }
 
 // JobSetStatus defines the observed state of JobSet
 type JobSetStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
 }
 
-//+kubebuilder:object:root=true
-//+kubebuilder:subresource:status
-
+// +kubebuilder:object:root=true
+// +kubebuilder:subresource:status
 // JobSet is the Schema for the jobsets API
 type JobSet struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-
-	Spec   JobSetSpec   `json:"spec,omitempty"`
-	Status JobSetStatus `json:"status,omitempty"`
+	Spec              JobSetSpec   `json:"spec,omitempty"`
+	Status            JobSetStatus `json:"status,omitempty"`
 }
 
-//+kubebuilder:object:root=true
-
+// +kubebuilder:object:root=true
 // JobSetList contains a list of JobSet
 type JobSetList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
 	Items           []JobSet `json:"items"`
+}
+type JobTemplate struct {
+	// Name is the name of the entry and will be used as a suffix
+	// for the Job name.
+	Name string `json:"name"`
+	// Template defines the template of the Job that will be created.
+	Template batchv1.JobTemplateSpec `json:"template,omitempty"`
+	// Network defines the networking options for the job.
+	Network *Network `json:"network"`
+}
+type Network struct {
+	// EnableDNSHostnames allows pods to be reached via their hostnames.
+	EnableDNSHostnames bool `json:"enableDNSHostnames"`
 }
 
 func init() {
