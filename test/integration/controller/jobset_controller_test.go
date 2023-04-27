39850304701811cdbc7423e32a1e1f8afed4d24c
Kevin Hannon <kannon1992@gmail.com>
2023-04-26 22:35:25 -0400

    Update test/integration/controller/jobset_controller_test.go
    Co-authored-by: Abdullah Gharaibeh <40361897+ahg-g@users.noreply.github.com>

--- test/integration/controller/jobset_controller_test.go
+++ test/integration/controller/jobset_controller_test.go
@@ -367,7 +367,7 @@ var _ = ginkgo.Describe("JobSet controller", func() {
 				},
 			},
 		}),
-		ginkgo.Entry("suspended jobset will not start", &testCase{
+		ginkgo.Entry("jobset created in suspended state", &testCase{
 			makeJobSet: func(ns *corev1.Namespace) *testing.JobSetWrapper {
 				return testJobSet(ns).
 					Suspend(true)
