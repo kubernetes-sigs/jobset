d7a06573e4545d117785db434d9fbadfc534dbfe
Kevin Hannon <kehannon@redhat.com>
2024-01-29 16:57:48 -0500

    linter fixes

--- pkg/controllers/jobset_controller.go
+++ pkg/controllers/jobset_controller.go
@@ -26,7 +26,6 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apiequality "k8s.io/apimachinery/pkg/api/equality"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/tools/record"
