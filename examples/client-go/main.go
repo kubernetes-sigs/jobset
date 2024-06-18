17b060e8d862aefe14eb00a51104add850e1ac0f
Daniel Vega-Myhre <danielvm@google.com>
2024-06-18 20:06:56 +0000

    add client-go example

---
+++ examples/client-go/main.go
@@ -0,0 +1,93 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/utils/ptr"
+
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
+	jobsetv1alpha2 "sigs.k8s.io/jobset/api/jobset/v1alpha2"
+	jobsetclient "sigs.k8s.io/jobset/client-go/clientset/versioned"
+	//+kubebuilder:scaffold:imports
+)
+
+var kubeconfig string
+
+func init() {
+	// kubeconfig file parsing
+	flag.StringVar(&kubeconfig, "kubeconfig", "", "path to Kubernetes config file")
+	flag.Parse()
+}
+
+func main() {
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+	client := jobsetclient.NewForConfigOrDie(config)
+
+	js, err := client.JobsetV1alpha2().JobSets("default").Create(ctx, &jobsetv1alpha2.JobSet{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-js",
+		},
+		Spec: jobsetv1alpha2.JobSetSpec{
+			ReplicatedJobs: []jobsetv1alpha2.ReplicatedJob{
+				{
+					Name: "rjob",
+					Template: batchv1.JobTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Name: "job",
+						},
+						Spec: batchv1.JobSpec{
+							Parallelism:  ptr.To(int32(2)),
+							Completions:  ptr.To(int32(2)),
+							BackoffLimit: ptr.To(int32(0)),
+							Template: corev1.PodTemplateSpec{
+								Spec: corev1.PodSpec{
+									Containers: []corev1.Container{
+										{
+											Name:    "bash-container",
+											Image:   "bash:latest",
+											Command: []string{"sleep", "60"},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}, metav1.CreateOptions{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("successfully created JobSet: %s\n", js.Name)
+}
