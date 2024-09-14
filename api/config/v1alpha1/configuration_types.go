d7c1dadd55906dec1d1275bcb7b73f08d1fa509f
RainFD <rainfd@live.cn>
2024-09-14 21:43:13 +0800

    feat: add component config (#609)
    * feat: add component config
    
    Signed-off-by: rainfd <rainfd@live.cn>
    
    * feat: override the file configuration with the command line flag
    
    Signed-off-by: rainfd <rainfd@live.cn>
    
    * fix: ignore network configuration tested by config
    
    ---------
    
    Signed-off-by: rainfd <rainfd@live.cn>

---
+++ api/config/v1alpha1/configuration_types.go
@@ -0,0 +1,133 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	configv1alpha1 "k8s.io/component-base/config/v1alpha1"
+)
+
+// +k8s:defaulter-gen=true
+// +kubebuilder:object:root=true
+
+// Configuration is the Schema for the configurations API
+type Configuration struct {
+	metav1.TypeMeta `json:",inline"`
+
+	// Namespace is the namespace in which jobset controller is deployed.
+	// It is used as part of DNSName of the webhook Service.
+	// If not set, the value is set from the file /var/run/secrets/kubernetes.io/serviceaccount/namespace
+	// If the file doesn't exist, default value is kueue-system.
+	Namespace *string `json:"namespace,omitempty"`
+
+	// ControllerManager returns the configurations for controllers
+	ControllerManager `json:",inline"`
+
+	// InternalCertManagerment is configuration for internalCertManagerment
+	InternalCertManagement *InternalCertManagement `json:"internalCertManagement,omitempty"`
+
+	ClientConnection *ClientConnection `json:"clientConnection,omitempty"`
+}
+
+type ControllerManager struct {
+	// Webhook contains the controllers webhook configuration
+	// +optional
+	Webhook ControllerWebhook `json:"webhook,omitempty"`
+
+	// LeaderElection is the LeaderElection config to be used when configuring
+	// the manager.Manager leader election
+	// +optional
+	LeaderElection *configv1alpha1.LeaderElectionConfiguration `json:"leaderElection,omitempty"`
+
+	// Metrics contains the controller metrics configuration
+	// +optional
+	Metrics ControllerMetrics `json:"metrics,omitempty"`
+
+	// Health contains the controller health configuration
+	// +optional
+	Health ControllerHealth `json:"health,omitempty"`
+}
+
+// ControllerWebhook defines the webhook server for the controller.
+type ControllerWebhook struct {
+	// Port is the port that the webhook server serves at.
+	// It is used to set webhook.Server.Port.
+	// +optional
+	Port *int `json:"port,omitempty"`
+
+	// Host is the hostname that the webhook server binds to.
+	// It is used to set webhook.Server.Host.
+	// +optional
+	Host string `json:"host,omitempty"`
+
+	// CertDir is the directory that contains the server key and certificate.
+	// if not set, webhook server would look up the server key and certificate in
+	// {TempDir}/k8s-webhook-server/serving-certs. The server key and certificate
+	// must be named tls.key and tls.crt, respectively.
+	// +optional
+	CertDir string `json:"certDir,omitempty"`
+}
+
+// ControllerMetrics defines the metrics configs.
+type ControllerMetrics struct {
+	// BindAddress is the TCP address that the controller should bind to
+	// for serving prometheus metrics.
+	// It can be set to "0" to disable the metrics serving.
+	// +optional
+	BindAddress string `json:"bindAddress,omitempty"`
+}
+
+// ControllerHealth defines the health configs.
+type ControllerHealth struct {
+	// HealthProbeBindAddress is the TCP address that the controller should bind to
+	// for serving health probes
+	// It can be set to "0" or "" to disable serving the health probe.
+	// +optional
+	HealthProbeBindAddress string `json:"healthProbeBindAddress,omitempty"`
+
+	// ReadinessEndpointName, defaults to "readyz"
+	// +optional
+	ReadinessEndpointName string `json:"readinessEndpointName,omitempty"`
+
+	// LivenessEndpointName, defaults to "healthz"
+	// +optional
+	LivenessEndpointName string `json:"livenessEndpointName,omitempty"`
+}
+
+type InternalCertManagement struct {
+	// Enable controls whether to enable internal cert management or not.
+	// Defaults to true. If you want to use a third-party management, e.g. cert-manager,
+	// set it to false. See the user guide for more information.
+	Enable *bool `json:"enable,omitempty"`
+
+	// WebhookServiceName is the name of the Service used as part of the DNSName.
+	// Defaults to jobset-webhook-service.
+	WebhookServiceName *string `json:"webhookServiceName,omitempty"`
+
+	// WebhookSecretName is the name of the Secret used to store CA and server certs.
+	// Defaults to jobset-webhook-server-cert.
+	WebhookSecretName *string `json:"webhookSecretName,omitempty"`
+}
+
+type ClientConnection struct {
+	// QPS controls the number of queries per second allowed for K8S api server
+	// connection.
+	QPS *float32 `json:"qps,omitempty"`
+
+	// Burst allows extra queries to accumulate when a client is exceeding its rate.
+	Burst *int32 `json:"burst,omitempty"`
+}
--- main.go
+++ main.go
@@ -33,10 +33,11 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
-	"sigs.k8s.io/controller-runtime/pkg/metrics/server"
-	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
 
+	configapi "sigs.k8s.io/jobset/api/config/v1alpha1"
 	jobset "sigs.k8s.io/jobset/api/jobset/v1alpha2"
+	"sigs.k8s.io/jobset/pkg/config"
 	"sigs.k8s.io/jobset/pkg/controllers"
 	"sigs.k8s.io/jobset/pkg/metrics"
 	"sigs.k8s.io/jobset/pkg/util/cert"
@@ -47,12 +48,15 @@ import (
 var (
 	scheme   = runtime.NewScheme()
 	setupLog = ctrl.Log.WithName("setup")
+	flagsSet = make(map[string]bool)
 )
 
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
 	utilruntime.Must(jobset.AddToScheme(scheme))
+	utilruntime.Must(configapi.AddToScheme(scheme))
+
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -63,6 +67,7 @@ func main() {
 	var qps float64
 	var burst int
 	var featureGates string
+	var configFile string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
@@ -71,6 +76,10 @@ func main() {
 	flag.Float64Var(&qps, "kube-api-qps", 500, "Maximum QPS to use while talking with Kubernetes API")
 	flag.IntVar(&burst, "kube-api-burst", 500, "Maximum burst for throttle while talking with Kubernetes API")
 	flag.StringVar(&featureGates, "feature-gates", "", "A set of key=value pairs that describe feature gates for alpha/experimental features.")
+	flag.StringVar(&configFile, "config", "",
+		"The controller will load its initial configuration from this file. "+
+			"Command-line flags will override any configurations set in this file. "+
+			"Omit this flag to use the default configuration values.")
 
 	opts := zap.Options{
 		Development: true,
@@ -78,11 +87,11 @@ func main() {
 	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
 
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	flag.Visit(func(f *flag.Flag) {
+		flagsSet[f.Name] = true
+	})
 
-	kubeConfig := ctrl.GetConfigOrDie()
-	kubeConfig.QPS = float32(qps)
-	kubeConfig.Burst = burst
+	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
 
 	if err := utilfeature.DefaultMutableFeatureGate.Set(featureGates); err != nil {
 		setupLog.Error(err, "Unable to set flag gates for known features")
@@ -91,30 +100,35 @@ func main() {
 
 	metrics.Register()
 
-	mgr, err := ctrl.NewManager(kubeConfig, ctrl.Options{
-		Scheme: scheme,
-		Metrics: server.Options{
-			BindAddress: metricsAddr,
-		},
-		WebhookServer: webhook.NewServer(
-			webhook.Options{
-				Port: 9443,
-			}),
-		HealthProbeBindAddress: probeAddr,
-		LeaderElection:         enableLeaderElection,
-		LeaderElectionID:       "6d4f6a47.x-k8s.io",
-		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
-		// when the Manager ends. This requires the binary to immediately end when the
-		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
-		// speeds up voluntary leader transitions as the new leader don't have to wait
-		// LeaseDuration time first.
-		//
-		// In the default scaffold provided, the program ends immediately after
-		// the manager stops, so would be fine to enable this option. However,
-		// if you are doing or is intended to do any operation such as perform cleanups
-		// after the manager stops then its usage might be unsafe.
-		// LeaderElectionReleaseOnCancel: true,
-	})
+	var options manager.Options
+
+	kubeConfig := ctrl.GetConfigOrDie()
+
+	options, cfg, err := apply(configFile)
+	if err != nil {
+		setupLog.Error(err, "Unable to load the configuration")
+		os.Exit(1)
+	}
+	kubeConfig.QPS = *cfg.ClientConnection.QPS
+	kubeConfig.Burst = int(*cfg.ClientConnection.Burst)
+
+	if flagsSet["metrics-bind-address"] {
+		options.Metrics.BindAddress = metricsAddr
+	}
+	if flagsSet["health-probe-bind-address"] {
+		options.HealthProbeBindAddress = probeAddr
+	}
+	if flagsSet["leader-elect"] {
+		options.LeaderElection = enableLeaderElection
+	}
+	if flagsSet["kube-api-qps"] {
+		kubeConfig.QPS = float32(qps)
+	}
+	if flagsSet["kube-api-burst"] {
+		kubeConfig.Burst = burst
+	}
+
+	mgr, err := ctrl.NewManager(kubeConfig, options)
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
@@ -217,4 +231,17 @@ func setupHealthzAndReadyzCheck(mgr ctrl.Manager, certsReady <-chan struct{}) {
 		setupLog.Error(err, "unable to set up ready check")
 		os.Exit(1)
 	}
+}
+
+func apply(configFile string) (ctrl.Options, configapi.Configuration, error) {
+	options, cfg, err := config.Load(scheme, configFile)
+	if err != nil {
+		return options, cfg, err
+	}
+	cfgStr, err := config.Encode(scheme, &cfg)
+	if err != nil {
+		return options, cfg, err
+	}
+	setupLog.Info("Successfully loaded configuration", "config", cfgStr)
+	return options, cfg, nil
 }
