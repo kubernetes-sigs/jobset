292c31659789f534da7aaf6c9bb128bbc7907ae6
Andrey Velichkevich <andrey.velichkevich@gmail.com>
2025-01-02 19:16:08 +0000

    Add integration tests for DependsOn CEL validation

--- api/jobset/v1alpha2/jobset_types.go
+++ api/jobset/v1alpha2/jobset_types.go
@@ -79,7 +79,7 @@ const (
 )
 
 // JobSetSpec defines the desired state of JobSet
-// +kubebuilder:validation:XValidation:rule="!(has(self.startupPolicy) && self.startupPolicy.startupPolicyOrder == 'InOrder' && self.replicatedJobs.all(x, has(x.dependsOn)))",message="StartupPolicy and DependsOn APIs are mutually exclusive"
+// +kubebuilder:validation:XValidation:rule="!(has(self.startupPolicy) && self.startupPolicy.startupPolicyOrder == 'InOrder' && self.replicatedJobs.exists(x, has(x.dependsOn)))",message="StartupPolicy and DependsOn APIs are mutually exclusive"
 // +kubebuilder:validation:XValidation:rule="!(has(self.replicatedJobs[0].dependsOn))",message="DependsOn can't be set for the first ReplicatedJob"
 type JobSetSpec struct {
 	// ReplicatedJobs is the group of jobs that will form the set.
