2e80b2490601b28f5f1652c4547fe66d7a1322b7
Arda Güçlü <aguclu@redhat.com>
2025-03-21 15:00:32 +0300

    Default to 8443 as metrics port to align service instead of 8080 (#844)
    * Default to 8443 as metrics port to align service instead of 8080
    
    * Add containerPort definitions in deployment

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func main() {
 	var burst int
 	var featureGates string
 	var configFile string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8443", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
